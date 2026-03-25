92b641c2b9824d24cea48c204360dcd72ebfa576
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-03-25 11:40:58 -0400

    Avoid creating a fake cuda library for IndeX as VisRTX requires the real one

--- projects/scripts/nvidiaindex.install.cmake
+++ projects/scripts/nvidiaindex.install.cmake
@@ -7,11 +7,3 @@ endif ()
 file(GLOB files "${source_dir}/lib/*")
 file(INSTALL ${files}
   DESTINATION "${install_dir}/${dir}")
-
-# Make a fake CUDA library. The installer will skip it, but one needs to be
-# found for the fixup_bundle scripts.
-if (WIN32)
-  file(WRITE "${install_dir}/${dir}/nvcuda.dll" "")
-else ()
-  file(WRITE "${install_dir}/${dir}/libcuda.so.1" "")
-endif ()
